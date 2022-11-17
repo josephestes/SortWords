9e4e49a9973042c5deb6805f7eee12c95ed8721c
Student <Student@DESKTOP-9LDA21V>
2022-11-17 15:30:20 -0500

    Add project files.

---
+++ SortWords/Program.cs
@@ -0,0 +1,36 @@
+﻿/* 
+ * This program accepts any number of words from the user, 
+ * sorts them in alphabetical order, and displays the sorted words separated by spaces
+ * 
+ * Program does not loop
+ */
+
+using System;
+using static System.Console;
+
+namespace SortWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompt user for input and send it to the SortAndDisplayWords method
+            WriteLine("Please enter words separated by spaces");
+            SortAndDisplayWords(ReadLine());
+        }
+        static void SortAndDisplayWords(string userInput)
+        {
+            //Split the input string into individual words and store them in an array
+            string[] words = userInput.Split();
+
+            //Sort the array in alphabetical order
+            Array.Sort(words);
+
+            //Display the sorted array elements separated by spaces
+            foreach (string word in words)
+            {
+                Write("{0} ", word);
+            }
+        }
+    }
+}
